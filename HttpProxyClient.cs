c1ec382373e2201387ae40e6f6e1644e61278a44
DanielDestino <daniel@desti.no>
2020-08-22 23:33:50 +0200

    Update HttpProxyClient.cs

--- HttpProxyClient.cs
+++ HttpProxyClient.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Server.Kestrel.Core.Features;
